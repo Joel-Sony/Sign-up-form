86e4a0ee6f0b804941d4dc3392ef2b3716916c7e
Joel <joelsony224@gmail.com>
2024-10-14 23:51:43 +0530

    Added password checking using regex

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const inputList = document.querySelectorAll("input");
 const errorList = document.querySelectorAll(".error");
 const emailRegex =/^\w+([.-]?\w+)*@\w+([.-]?\w+)(\.\w{2,3})+$/
 const phoneRegex=/[0-9]+/
+const passRegex=/\w{8,}/
+
 console.table(inputList);
 inputList[2].addEventListener('input',()=>{
     if(!emailRegex.test(inputList[2].value)){
@@ -18,5 +20,14 @@ inputList[3].addEventListener('input',()=>{
     }
     else{
         errorList[3].textContent='';
+    }
+})
+
+inputList[4].addEventListener('input',()=>{
+    if(!passRegex.test(inputList[4].value)){
+        errorList[4].textContent='Minimum 8 characters';
+    }
+    else{
+        errorList[4].textContent='';
     }
 })
